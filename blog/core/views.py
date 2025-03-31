84695466a191d4485becdd0da40341a71ff6695e
Yuvraj Rathi <yrathi66@gmail.com>
2025-03-31 15:43:39 +0530

    remove, start fresh

--- blog/core/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def frontpage(request):
-      return render(request, 'core/frontpage.html')
-
-def about(request):
-      return render(request, 'core/about.html')
